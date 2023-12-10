7daebdc5efdaf5dab9a4da595023e949f2679130
sidharth024gb <sidharthgb@gmail.com>
2023-12-10 22:50:54 +0530

    Updated Two_Sum

--- Two_Sum.java
+++ Two_Sum.java
@@ -1,45 +1,47 @@
-import java.util.Scanner;  
-import java.util.HashMap; 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Scanner; 
 
 
 public class Two_Sum{
     public static void main(String[] args){
-        int target,i=0,n;
-        int[] final_out = new int[2];
+        int target,i,n;
         
-        Scanner sc = new Scanner(System.in);
-
+        Scanner read = new Scanner(System.in); 
         System.out.println("Enter the array elemrnt count:");
-        n = sc.nextInt();
+        n = read.nextInt();
 
         int[] array = new int[n];
 
         System.out.println("Enter the array elements:");
         for(i=0;i<n;i++){
-            array[i]=sc.nextInt();
+            array[i]=read.nextInt();
         }
 
         System.out.println("Enter the target:");
-        target = sc.nextInt();
+        target = read.nextInt();
 
         Solution s = new Solution();
-        final_out [0]= s.twoSum(array,target);
-        System.out.println(final_out[0]);
+        s.twoSum(array,target);
+        
     }
 }
 class Solution{
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> seen = new HashMap<Integer,Integer>();
-        //int[] array = new int[2];
+        int[] array = new int[2];
         for(int i=0;i<nums.length;i++){
             int current_value = nums[i];
             int find_value = target - current_value;
 
             if(seen.containsKey(find_value)){
-                return new int[] {seen.get(find_value),i};
-                //array[0]=seen.get(find_value);
-                //array[1]=i;
-                //System.out.println(array);
+                //return new int[] {seen.get(find_value),i};//leetcode
+                array[0]=seen.get(find_value);
+                array[1]=i;
+                //System.out.println(array);//failed
+                //System.out.println(array[0]);//success but not ok
+                //System.out.println(array[1]);//success but not ok
+                System.out.println(Arrays.toString(array));//Success
             }
             else{
                 seen.put(nums[i],i);
