48b7271bc8fb593872b47766956d768e677220da
sidharth024gb <sidharthgb@gmail.com>
2023-12-21 15:50:19 +0530

    read closed

--- Two_Sum.java
+++ Two_Sum.java
@@ -4,45 +4,48 @@ import java.util.Scanner;
 
 
 public class Two_Sum{
-    public static void main(String[] args){
-        int target,i,n;
-        
+    public static void main(String[] args) {
         Scanner read = new Scanner(System.in); 
-        System.out.println("Enter the array elemrnt count:");
-        n = read.nextInt();
-
-        int[] array = new int[n];
-
-        System.out.println("Enter the array elements:");
-        for(i=0;i<n;i++){
-            array[i]=read.nextInt();
-        }
-
-        System.out.println("Enter the target:");
-        target = read.nextInt();
-
-        Solution s = new Solution();
-        s.twoSum(array, target);
+        try {
+            
+            int target,i,n;
         
-        read.close();
+            System.out.println("Enter the array elemrnt count:");
+            n = read.nextInt();
+
+            int[] array = new int[n];
+
+            System.out.println("Enter the array elements:");
+            for(i=0;i<n;i++){
+                array[i]=read.nextInt();
+            }
+
+            System.out.println("Enter the target:");
+            target = read.nextInt();
+
+            Solution s = new Solution();
+            int[] result = s.twoSum(array, target);
+            if (result != null) {
+                System.out.println("Indices of the two numbers: " + Arrays.toString(result));
+            } 
+            else {
+            System.out.println("No two numbers found that sum up to the target.");
+            }
+        }
+        finally {
+            read.close();
+        }
     }
 }
 class Solution{
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer,Integer> seen = new HashMap<Integer,Integer>();
-        int[] array = new int[2];
         for(int i=0;i<nums.length;i++){
             int current_value = nums[i];
             int find_value = target - current_value;
 
             if(seen.containsKey(find_value)){
-                //return new int[] {seen.get(find_value),i};//leetcode
-                array[0]=seen.get(find_value);
-                array[1]=i;
-                //System.out.println(array);//failed
-                //System.out.println(array[0]);//success but not ok
-                //System.out.println(array[1]);//success but not ok
-                System.out.println(Arrays.toString(array));//Success
+                return new int[]{seen.get(find_value), i};
             }
             else{
                 seen.put(nums[i],i);
